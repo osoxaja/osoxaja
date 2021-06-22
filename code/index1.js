6fb1a58a1ef3950ee3abbb75df14b93dc7fbfbbb
osoxaja <85166498+osoxaja@users.noreply.github.com>
2021-06-22 16:30:38 +0300

    Update index1.js

--- code/index1.js
+++ code/index1.js
@@ -1,41 +1,46 @@
-function formatDate(date) {
-  return date.toLocaleDateString();
-}
+import React, { Component } from 'react'
+import Carousel from 'react-bootstrap/Carousel'
+import lakeImg from '../assets/lake.jpg';
+import ntownImg from '../assets/ntown.jpg';
+export default class CarouselBox extends Component {
+    render(){
+        return (
+            <Carousel>
+                <Carousel.Item>
+                    <img
+                       ClassName="d-block w-100"
+                       src={ lakeImg }
+                       alt="Lake"                   
+                    />
+                    <Carousel.Caption>
+                        <h3>Lake Image</h3>
+                        <p>This lake beautifull isn't it?</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                       ClassName="d-block w-100"
+                       src={ ntownImg }
+                       alt="Town"                   
+                    />
+                    <Carousel.Caption>
+                        <h3>Town Image</h3>
+                        <p>This town beautifull isn't it?</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                       ClassName="d-block w-100"
+                       src={ lakeImg }
+                       alt="Lake"                   
+                    />
+                    <Carousel.Caption>
+                        <h3>Lake Image</h3>
+                        <p>This lake beautifull isn't it?</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+            </Carousel>
+        )
+    }
 
-function Comment(props) {
-  return (
-    <div className="Comment">
-      <div className="UserInfo">
-        <img
-          className="Avatar"
-          src={props.author.avatarUrl}
-          alt={props.author.name}
-        />
-        <div className="UserInfo-name">
-          {props.author.name}
-        </div>
-      </div>
-      <div className="Comment-text">{props.text}</div>
-      <div className="Comment-date">
-        {formatDate(props.date)}
-      </div>
-    </div>
-  );
-}
-
-const comment = {
-  date: new Date(),
-  text: 'I hope you have a nice day',
-  author: {
-    name: 'Hello Kitty',
-    avatarUrl: 'https://placekitten.com/g/64/64',
-  },
-};
-ReactDOM.render(
-  <Comment
-    date={comment.date}
-    text={comment.text}
-    author={comment.author}
-  />,
-  document.getElementById('root')
-);
+ }
